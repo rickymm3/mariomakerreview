19870119b2fc31ed707bbbb908c60e1413b4a81e
rickymm3 <rickymm3@gmail.com>
2015-05-19 16:26:18 -0400

    removed redundent admin check

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,17 +1,9 @@
 class Admin::BaseController < ApplicationController
 
-  before_filter :require_admin
   layout "admin"
 
   def index
   end
 
-  protected
-
-  def require_admin
-    unless current_user.try(:is_admin?)
-      render404 and return false
-    end
-  end
 
 end
